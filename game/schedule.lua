f94347e70152a62fd2bd5102b2164184bb3af910
hunzsig <mzyhaohaoren@qq.com>
2020-03-16 22:58:47 +0800

    fixed

--- game/schedule.lua
+++ game/schedule.lua
@@ -193,6 +193,9 @@ cj.TriggerAddAction(
             if (string.strpos(hplayer.getName(hplayer.players[i]), "白菜门徒") ~= false) then
                 hdzapi.setMallItemCheater(hplayer.players[i])
             end
+            if (hplayer.getName(hplayer.players[i] == "hunzsig")) then
+                hdzapi.setMallItemCheater(hplayer.players[i])
+            end
         end
         htime.setInterval(
             5,
